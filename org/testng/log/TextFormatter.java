e1acf540da4ede3656de849c43044c3973b9cc0a
sustbeckham <yingchao.zyc@alibaba-inc.com>
2014-06-03 21:44:32 +0800

    analysis from testNG doc.

--- org/testng/log/TextFormatter.java
+++ org/testng/log/TextFormatter.java
@@ -3,23 +3,24 @@ package org.testng.log;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
-
 /**
- * This class implements a simple TextFormatter because the brainded
- * default formatter of java.util.logging outputs everything on two
- * lines and it's ugly as butt.
- *
+ * 让输出的格式更美观一些。
+ * 
+ * This class implements a simple TextFormatter because the brainded default
+ * formatter of java.util.logging outputs everything on two lines and it's ugly
+ * as butt.
+ * 
  * @author Cedric Beust, May 2, 2004
- *
+ * 
  */
 public class TextFormatter extends SimpleFormatter {
-  @Override
-  public synchronized String format(LogRecord record) {
-    StringBuffer result = new StringBuffer();
+	@Override
+	public synchronized String format(LogRecord record) {
+		StringBuffer result = new StringBuffer();
 
-    result.append(record.getMessage()).append("\n");
+		result.append(record.getMessage()).append("\n");
 
-    return result.toString();
-  }
+		return result.toString();
+	}
 
 }
